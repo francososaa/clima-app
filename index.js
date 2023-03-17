d24101bc694bd5cff98bd7c381513f501b44a855
Franco Sosa <soosaf22@gmail.com>
2023-03-17 13:43:31 -0300

    Primer Commit

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
 
+require('colors');
+const { inquirerMenu, 
+        pausa,
+        leerInput,
+        listadoTareasBorrar,
+        confirmar,
+        mostrarListadoChecklist
+} = require('./helpers/inquirer');
+
+const main = async() => {
+
+    let opt = '';
+
+    do {
+        opt = await inquirerMenu();
+
+        switch ( opt ){
+            case '1': 
+                
+            break;
+
+            case '2': 
+                
+            break;
+
+        }
+        await pausa();
+        
+    } while (opt !=='0');
+    
+}
+
+main();
